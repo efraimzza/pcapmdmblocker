1ae3cfb446b2918006afa8cf8804be9c14e6e56f
IsraelBer <105712394+IsraelBer@users.noreply.github.com>
2025-12-23 11:36:14 +0200

    מתן אפשרות לקבלת הרשאת הניהול בירושה מאפליקציה אחרת
    Added logging for device admin enable/disable events and ownership transfer completion.
    
    הסבר:
    אפליקציות ניהול חינמיות רבות, ביניהם:
    test dpc. owndroid. offline mdm. dhizuku. תומכות בהעברת הבעלות מאחת לשניה.
    
    אך עד כה כל אפליקציות החסימה שפותחו, לא תמכו בקבלת הרשאת הניהול בירושה.
    
    הדבר יעיל לצורך:
    1 אפשר להגדיר הגדרות שקיימות רק באפליקציות האחרות, לדוגמה ב test dpc, ואחר כך כשתעבירו בעלות, ההגדרות הללו ישארו!
    
    2 את האפליקציה test dpc ניתן להתקין אחרי איפוס ללא צורך במצלמה או בחיבור למחשב, פשוט בוחרים במסך הפתיחה להתחבר לחשבון בגוגל, ובמקום כתובת אימייל, מקלידים afw#testdpc ומיד האפליקציה מותקנת ומוגדרת כמנהל מכשיר, ואז נכנסים, מחפשים transfer, לוחצים, בוחרים בגרסת האיי בלוק המצורפת, (אחרי שהתקנתם כמובן)
    
    שימו לב, שאפשרות העברת ניהול נתמכת רק מאנדרואיד 9 והלאה.
    בהצלחה!

--- app/src/main/java/com/emanuelef/remote_capture/activities/admin.java
+++ app/src/main/java/com/emanuelef/remote_capture/activities/admin.java
@@ -3,17 +3,33 @@ package com.emanuelef.remote_capture.activities;
 import android.app.admin.DeviceAdminReceiver;
 import android.content.Intent;
 import android.content.Context;
+import android.os.PersistableBundle;
+import android.util.Log;
 
 public class admin extends DeviceAdminReceiver {
+
+    private static final String TAG = "PCAPdroid_Admin";
 
     @Override
     public void onEnabled(Context context, Intent intent) {
         super.onEnabled(context, intent);
+        Log.d(TAG, "Device Admin Enabled");
     }
 
     @Override
     public void onDisabled(Context context, Intent intent) {
         super.onDisabled(context, intent);
+        Log.d(TAG, "Device Admin Disabled");
     }
-    
+
+    /**
+     * פונקציה זו נקראת ברגע שהעברת הבעלות לאפליקציה זו הושלמה.
+     */
+    @Override
+    public void onTransferOwnershipComplete(Context context, PersistableBundle bundle) {
+        super.onTransferOwnershipComplete(context, bundle);
+        Log.d(TAG, "Transfer Ownership Completed Successfully!");
+        
+        // כאן ניתן להוסיף לוגיקה נוספת להפעלת הגדרות מיד עם קבלת הניהול
+    }
 }
